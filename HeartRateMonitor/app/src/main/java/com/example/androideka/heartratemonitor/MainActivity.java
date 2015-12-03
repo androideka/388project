67246d3b654e29cc006586b179493c52d1ab1cc4
mattrose <mattrose@iastate.edu>
2015-12-03 16:54:19 -0600

    STUPID JAVA_HOME

--- HeartRateMonitor/app/src/main/java/com/example/androideka/heartratemonitor/MainActivity.java
+++ HeartRateMonitor/app/src/main/java/com/example/androideka/heartratemonitor/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.androideka.heartratemonitor;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,20 +18,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.UUID;
 
+import zephyr.android.HxMBT.BTClient;
+import zephyr.android.HxMBT.BTComms;
+import zephyr.android.HxMBT.ConnectedEvent;
+import zephyr.android.HxMBT.ConnectedListener;
+import zephyr.android.HxMBT.ZephyrPacket;
+import zephyr.android.HxMBT.ZephyrPacketArgs;
+import zephyr.android.HxMBT.ZephyrProtocol;
+
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
     private InputStream inputStream;
+    private BluetoothSocket socket;
+    private boolean connected = false;
 
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
@@ -44,33 +48,22 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("NAME", "" + device.getName());
                 Log.d("ADDRESS", "" + device.getAddress());
                 Toast.makeText(context, "" + device.getName(), Toast.LENGTH_LONG).show();
-                if (device.getName() != null && device.getName().equals("HXM022879")) {
+                if (device.getName() != null && device.getName().equals("HXM021780")) {
                     try {
                         bluetooth.cancelDiscovery();
-                        long msb = 10;
-                        long lsb = 11;
-                        UUID uuid = new UUID(msb, lsb);
-
-                        Method method = device.getClass().getMethod("setPin", byte[].class);
-                        byte[] pin = {(byte) 1234};
-                        method.invoke(device, pin);
-                        device.getClass().getMethod("setPairingConfirmation",
-                                boolean.class).invoke(device, true);
+                        byte[] pin = {1,2,3,4};
+                        device.setPairingConfirmation(true);
+                        device.setPin(pin);
                         device.getClass().getMethod("cancelPairingUserInput").invoke(device);
-                        BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid);
-                        socket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket",
-                                new Class[]{int.class}).invoke(device,1);
+                        // Set port to 1, default is -1
+                        socket = (BluetoothSocket) device.getClass()
+                                .getMethod("createRfcommSocket", new Class[]{int.class})
+                                .invoke(device,1);
                         Log.d("BLUETOOTH", "Connecting to socket.....");
                         socket.connect();
                         Log.d("BLUETOOTH", "Socket connected");
-                        monitorHeartRate(socket);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    } catch (NoSuchMethodException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
+                        monitorHeartRate();
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -84,15 +77,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         if (bluetooth != null && !bluetooth.isEnabled()) {
             Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -140,12 +124,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void monitorHeartRate(BluetoothSocket socket) throws IOException {
+    public void monitor(View view) throws Exception {
+        monitorHeartRate();
+    }
+
+    public void monitorHeartRate() throws Exception {
         Log.d("SUCCESS", "Successfully opened a socket to the heart rate monitor.");
         inputStream = socket.getInputStream();
+        ZephyrPacket packet = new ZephyrPacket();
+        byte[] buffer = new byte[60];
         while(true)
-        {socket 
-            System.out.println("" + inputStream.read());
+        {
+            packet.Serialize(buffer);
+            ZephyrPacketArgs args = packet.Parse(buffer);
+            Log.d("Bytes received: ", "" + args.getNumRvcdBytes());
+            Log.d("Message ID: ", "" + args.getMsgID());
+            Log.d("Status: ", "" + args.getStatus());
+            Log.d("", "" + args.getBytes());
         }
     }
 
